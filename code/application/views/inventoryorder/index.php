f76d271b8a7e79c263491c7fbb58cef36c4a83e1
刘世群 <lsqandzy@hotmail.com>
2019-05-26 21:16:43 +0800

    入库单变更

--- code/application/views/inventoryorder/index.php
+++ code/application/views/inventoryorder/index.php
@@ -1,3 +1,101 @@
+<div class="ibox-content m-b-sm border-bottom">
+    <div class="row">
+        <div class="col-sm-2">
+            <div class="form-group">
+                <label class="control-label" for="product_name">品类</label>
+                <input type="text" id="product_name" name="product_name" value="" placeholder="产品名称" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-2">
+            <div class="form-group">
+                <label class="control-label" for="price">货号</label>
+                <input type="text" id="price" name="price" value="" placeholder="00.00" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-2">
+            <div class="form-group">
+                <label class="control-label" for="quantity">品名</label>
+                <input type="text" id="quantity" name="quantity" value="" placeholder="123" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-2">
+            <div class="form-group">
+                <label class="control-label" for="quantity">颜色</label>
+                <input type="text" id="quantity" name="quantity" value="" placeholder="123" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-2">
+            <div class="form-group">
+                <label class="control-label" for="quantity">数量</label>
+                <input type="text" id="quantity" name="quantity" value="" placeholder="123" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-2">
+            <div class="form-group">
+                <label class="control-label" for="quantity">单价</label>
+                <input type="text" id="quantity" name="quantity" value="" placeholder="123" class="form-control">
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="form-group">
+                <label class="control-label" for="product_name">产品名称</label>
+                <input type="text" id="product_name" name="product_name" value="" placeholder="产品名称" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-2">
+            <div class="form-group">
+                <label class="control-label" for="price">价格</label>
+                <input type="text" id="price" name="price" value="" placeholder="00.00" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-2">
+            <div class="form-group">
+                <label class="control-label" for="quantity">数量</label>
+                <input type="text" id="quantity" name="quantity" value="" placeholder="123" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="form-group">
+                <label class="control-label" for="status">状态</label>
+                <select name="status" id="status" class="form-control">
+                    <option value="1" selected="">启用</option>
+                    <option value="0">关闭</option>
+                </select>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="form-group">
+                <label class="control-label" for="product_name">产品名称</label>
+                <input type="text" id="product_name" name="product_name" value="" placeholder="产品名称" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-2">
+            <div class="form-group">
+                <label class="control-label" for="price">价格</label>
+                <input type="text" id="price" name="price" value="" placeholder="00.00" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-2">
+            <div class="form-group">
+                <label class="control-label" for="quantity">数量</label>
+                <input type="text" id="quantity" name="quantity" value="" placeholder="123" class="form-control">
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <div class="form-group">
+                <label class="control-label" for="status">状态</label>
+                <select name="status" id="status" class="form-control">
+                    <option value="1" selected="">启用</option>
+                    <option value="0">关闭</option>
+                </select>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="row">    
     <div class="col-lg-12">
         <div class="ibox float-e-margins">
--- code/public/application/js/website.inventoryorder.js
+++ code/public/application/js/website.inventoryorder.js
@@ -1,86 +1,46 @@
 $.fn.dataTable.ext.errMode = 'throw';//设置DataTables的错误提示方式为抛出异常 
 
-var d=[
-{code:'1234','name':'T','type':'T1','color':['黑色','白色'],'size':['M','L','XL']},
-{code:'1235','name':'T','type':'T1','color':['黑色','白色'],'size':['M','L','XL']},
-];
 
-var cc=[];
-function resetData(){
-    cc=[];
-    for(var i in d){
-      //算出有几个颜色
-      var codecount=d[i]['color'].length;
-      for(var color in d[i]['color']){
-        //该颜色下有几个尺寸
-        var sizecount=d[i]['size'].length;
-        codecount=codecount*sizecount;
-        for(var size in d[i]['size']){      
-          cc.push({            
-            'code':d[i]['code'],
-            'name':d[i]['name'],
-            'type':d[i]['type'],
-            'color':d[i]['color'][color],
-            'size':d[i]['size'][size],
-            'number':0,
-            'sizecount':sizecount,
-            'codecount':codecount,
-          });         
-          sizecount=0;
-          codecount=0;
-        }
-      }
-    }
-}
-resetData();
+//重置
 function delrow(t,state){
   var row_data = table.row(t.parentNode).data();
-  //cc.splice(row_data.id,1)
-  //这里得去d数组删除 重新组建cc数组
-  for(var i in d){
-    if(row_data['code']==d[i]['code']){
-      switch(state){
-        case "code":
-            d.splice(i,1);
-            break;
-        case "color":
-          break;
-        case "size":
-          break;
-      }
-      break;
-    }
-  }
-  resetData();
+  
   initTable();
   //table.ajax.reload(); 
 }
-console.log(cc);
+
+var tData=[{"code":"1234","name":"T","type":"T1","color":"黑色","size":"M","number":0,"sizecount":3,"codecount":6},{"code":"1234","name":"T","type":"T1","color":"黑色","size":"L","number":0,"sizecount":0,"codecount":0},{"code":"1234","name":"T","type":"T1","color":"黑色","size":"XL","number":0,"sizecount":0,"codecount":0},{"code":"1234","name":"T","type":"T1","color":"白色","size":"M","number":0,"sizecount":3,"codecount":0},{"code":"1234","name":"T","type":"T1","color":"白色","size":"L","number":0,"sizecount":0,"codecount":0},{"code":"1234","name":"T","type":"T1","color":"白色","size":"XL","number":0,"sizecount":0,"codecount":0},{"code":"1235","name":"T","type":"T1","color":"黑色","size":"M","number":0,"sizecount":3,"codecount":6},{"code":"1235","name":"T","type":"T1","color":"黑色","size":"L","number":0,"sizecount":0,"codecount":0},{"code":"1235","name":"T","type":"T1","color":"黑色","size":"XL","number":0,"sizecount":0,"codecount":0},{"code":"1235","name":"T","type":"T1","color":"白色","size":"M","number":0,"sizecount":3,"codecount":0},{"code":"1235","name":"T","type":"T1","color":"白色","size":"L","number":0,"sizecount":0,"codecount":0},{"code":"1235","name":"T","type":"T1","color":"白色","size":"XL","number":0,"sizecount":0,"codecount":0}];
+
 var table;
 function initTable(){
    table=$('.dataTables-inventoryorder').DataTable({
       "destroy": true,
-        "data": cc,  
+        "data": tData,  
         'pageLength': 10000,      
         "dom":'it', 
         "oLanguage": {
              "sInfo": "共 _TOTAL_条数据",           
         },       
         "columns": [
-            {"data": null, class:'center',    title: "序号",     "width": "8%"},            
-            {"data": "code", class:'center',title: "货号", "width": "15%"},                 
+            {"data": null, class:'center',    title: "操作", "width": "8%"},            
+            {"data": "code", class:'center',title: "货号", "width": "15%"},     
+            {"data": "type",class:'center',title: "品类",   "width": "15%"},
+            {"data": "name",class:'center',title: "品名",   "width": "15%"},            
             {"data": "color",class:'center',title: "颜色",   "width": "15%"},
             {"data": "size",class:'center',title: "尺寸",   "width": "40%"},                      
         ],
         "fnDrawCallback": function () {
             this.api().column(0).nodes().each(function (cell, i) {
-                cell.innerHTML = i + 1;
+                //cell.innerHTML = i + 1;
+                cell.innerHTML ="<span class='btn btn-link' onclick='delrow(this)'><i class='fa fa-trash-o'></i>删</span>";
+                
             });
         },
         "columnDefs": [
           {     //重要的部分专门处理货号、大类、品名的合并
               targets: [1], //要合并的列数
               createdCell: function (td, cellData, rowData, row, col) { 
+                  return;
                   var rowspan = rowData.codecount;//这里主要是利用了数据中的codecount来控制接下来要合并的行数
                   if (rowspan > 1) { //相同的行这里codecount是0
                       $(td).attr('rowspan', rowspan);
@@ -89,16 +49,11 @@ function initTable(){
                   if (rowspan == 0) {
                       $(td).remove();  //单元格移除掉 留给上一行合并
                   }
-              },
-              "render": function (data, type, rowData) {
-                 var str= "<span class='label'>货号:{code}</span><br/><br/><span  class='label label-success'>品名:{name}</span><br/><br/><span  class='label label-info'>品类:{type}</span>"
-                 .format({'code':rowData['code'],'name':rowData['name'],'type':rowData['type']});
-                 str+="<br/><br/><span class='btn btn-link' onclick='delrow(this,\"code\")'><i class='fa fa-trash-o'></i>删</span>";
-                 return str;
-              }  
+              }             
           },{     //重要的部分专门处理颜色的合并
               targets: [2], //要合并的列数
               createdCell: function (td, cellData, rowData, row, col) { 
+                  return;
                   var rowspan = rowData.sizecount;//这里主要是利用了数据中的sizecount来控制接下来要合并的行数
                   if (rowspan > 1) { //相同的行这里sizecount是0
                       $(td).attr('rowspan', rowspan);
@@ -109,8 +64,7 @@ function initTable(){
                   }
               },
               "render": function (data, type, rowData) {
-                  return "<span title='" + data + "'>" + data + "</span>"+
-                  '<span class="btn btn-link" onclick="delrow(this,\'color\')"><i class="fa fa-trash-o "></i>删</span>';
+                  return "<span title='" + data + "'>" + data + "</span>";
               }  
           },{   
               targets: [-1],  //处理尺寸            
@@ -118,8 +72,7 @@ function initTable(){
                 return '<div class="input-group m-b">'+
                   '<span class="input-group-addon">'+data+'</span> '+
                   '<input type="text" placeholder="" class="form-control" value="'+rowData.number+'">'+
-                '<span class="input-group-addon">件</span>'+
-                '<span class="input-group-addon btn btn-info" onclick="delrow(this.parentNode,\'size\')"><i class="fa fa-trash-o "></i>删</span></div>';                 
+                '<span class="input-group-addon">件</span></div>';                 
               }  
           }]      
     });
